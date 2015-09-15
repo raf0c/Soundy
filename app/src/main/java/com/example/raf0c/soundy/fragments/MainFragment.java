1e1501ee50dc3dda06cb53e6d34f1114daee8c1c
Rafael Garcia <ragampo@gmail.com>
2015-09-14 20:23:29 -0400

    mainfragment class, declaration static newInstance

---
+++ app/src/main/java/com/example/raf0c/soundy/fragments/MainFragment.java
@@ -0,0 +1,23 @@
+package com.example.raf0c.soundy.fragments;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by raf0c on 14/09/15.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment(){
+
+    }
+
+    public static Fragment newInstance(Context context) {
+        MainFragment f = new MainFragment();
+
+        return f;
+    }
+
+
+
+}
